6936b930be1d3ca42143e8850bd27283e5d47908
Pks-12 <153657149+Pks-12@users.noreply.github.com>
2024-05-31 11:31:25 +0530

    Update textinput.js

--- textinput.js
+++ textinput.js
@@ -2,7 +2,8 @@ let inputbox = document.querySelector("#inputbox");
 let btn3 = document.querySelector("#click");
 let btn2= document.querySelector("#click2");
 let btn4 = document.querySelector("#click3");
-
+let btn5 = document.querySelector("#click4");
+let displayArea = document.getElementById("displayArea");
 btn3.addEventListener('click',function(){
     inputbox.value=inputbox.value.toUpperCase();
 })
@@ -16,6 +17,15 @@ btn4.addEventListener('click',function(e){
     inputbox.setSelectionRange(0, 99999);
     document.execCommand('copy');
 })
+
+btn5.addEventListener('click', function() {
+    event.preventDefault();
+    const textToAdd = inputbox.value; // Get the value of the input box
+    const paragraph = document.createElement('p');// Create a new paragraph element
+    paragraph.textContent = textToAdd; // Set the text content of the paragraph
+    displayArea.appendChild(paragraph); // Append the paragraph to the display area
+    inputbox.value = '';// Clear the input box
+});
 
 
 
